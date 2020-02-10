4482e546c3ce2291b928174ea8f5cd50325afbd1
ninad99 <ninuch7@gmail.com>
2020-02-11 01:15:24 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+const admin = require('firebase-admin');
+const serviceAccount = require('./srijan20-admin.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const app = express();
+const baseURL = 'http://localhost:2000';
+
+app.use(cors());
+app.use(bodyParser.json({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', 'views');
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res, next) => {
+  res.send('<html><head></head><body><h2>Server running!</h2></body></html>');
+});
+
+module.exports = {
+  firestore: admin.firestore(),
+  baseURL: baseURL
+}
+
+const merchandiseRoutes = require('./routes/merchandise');
+const workshopsRoutes = require('./routes/workshops');
+app.use('/merch', merchandiseRoutes);
+app.use('/workshops', workshopsRoutes);
+
+app.listen(process.env.PORT || 2000, () => console.log(`server running on port ${process.env.PORT ? process.env.PORT : 2000}`));
---
+++ routes/workshops.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const firestore = require('../app').firestore;
+const paytmConfig = require('../config/config');
+const checksum_lib = require('../paytm/checksum');
+const baseURL = require('../app').baseURL;
+const router = express.Router();
+
+router.get('/txn', (req, res, next) => {
+  const { orderId, amount } = req.query;
+  console.log(req.query);
+  res.render('txn.ejs', { config: {
+      ORDER_ID: orderId,
+      TXN_AMOUNT: amount
+    },
+    title: 'Srijan 20 workshop payment'
+  });
+});
+
+router.post('/txn', (req, res, next) => {
+  const body = req.body;
+
+  firestore.collection('workshopOrders').doc(body['ORDER_ID']).get()
+    .then(docSnapshot => {
+      const params = {};
+      const data = docSnapshot.data();
+      params['MID'] = paytmConfig.MID;
+      params['ORDER_ID'] = body['ORDER_ID'];
+      params['WEBSITE'] = paytmConfig.WEBSITE;
+      params['CHANNEL_ID'] = paytmConfig.CHANNEL_ID;
+      params['INDUSTRY_TYPE_ID'] = paytmConfig.INDUSTRY_TYPE_ID;
+      params['TXN_AMOUNT']	= body['TXN_AMOUNT'];
+      params['CUST_ID'] = data.userId;
+      params['EMAIL']	= data.email;
+      params['MOBILE_NO']	= data.phoneNo;
+      params['CALLBACK_URL'] = baseURL + '/workshops/response';
+
+      console.log('params: ', params, 'merchant key:', paytmConfig.PAYTM_MERCHANT_KEY);
+      checksum_lib.genchecksum(params, paytmConfig.PAYTM_MERCHANT_KEY, (err, checksum) => {
+        if (!err) {
+          res.render('pageRedirect.ejs', {
+            params: params,
+            checksum: checksum,
+            url: paytmConfig.PAYTM_TRANSACTION_URL
+          });
+        }
+      });
+    })
+});
+
+router.post('/response', async (req, res, next) => {
+  const paramList = req.body;
+  console.log(paramList);
+  if (checksum_lib.verifychecksum(paramList, paytmConfig.PAYTM_MERCHANT_KEY, paramList.CHECKSUMHASH)) {
+    if (paramList.STATUS === 'TXN_SUCCESS') {
+      const orderRef = await firestore.collection('workshopOrders').doc(paramList.ORDERID).get();
+      const orderData = orderRef.data();
+      firestore.collection('workshopOrders').doc(paramList.ORDERID).update({
+        ...orderData,
+        paymentStatus: 'paid'
+      })
+      res.render('response.ejs', { valid: true });
+    } else if (paramList.STATUS === 'TXN_FAILURE') {
+      const orderRef = await firestore.collection('workshopOrders').doc(paramList.ORDERID).get();
+      const orderData = orderRef.data();
+      firestore.collection('workshopOrders').doc(paramList.ORDERID).update({
+        ...orderData,
+        paymentStatus: 'failed'
+      })
+      res.render('response.ejs', { valid: false, respMsg: paramList.RESPMSG });
+    }
+  } else {
+    const orderRef = await firestore.collection('workshopOrders').doc(paramList.ORDERID).get();
+    const orderData = orderRef.data();
+    firestore.collection('workshopOrders').doc(paramList.ORDERID).update({
+      ...orderData,
+      paymentStatus: 'failed'
+    })
+    res.render('response.ejs', { valid: false, respMsg: 'An error occured while verifying the transaction. Please go back and try again.' });
+  }
+});
+
+module.exports = router;
